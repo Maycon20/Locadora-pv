4e18542397723cc9a8dde965bc074ef4735885bf
Bett3 <57967702+Bett3@users.noreply.github.com>
2019-12-14 14:27:40 -0300

    Conexão e ExecuteSQL

--- src/DAO/Conexao.java
+++ src/DAO/Conexao.java
@@ -5,10 +5,35 @@
  */
 package DAO;
 
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Betty
  */
 public class Conexao {
+    public static Connection AbrirConexao(){
+        Connection con = null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost/locadora";
+            
+            con = (Connection) DriverManager.getConnection(url,"root","");
+        } catch(Exception e){
+            JOptionPane.showMessageDialog(null,"Erro ao conectar ao banco", "Video locadora", JOptionPane.ERROR_MESSAGE);
+            e.getMessage();
+        }
+
+        return con;
+    }
     
+    public static void FecharConexao(Connection con){
+        try{
+            con.close();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- src/DAO/ExecuteSQL.java
+++ src/DAO/ExecuteSQL.java
@@ -5,10 +5,24 @@
  */
 package DAO;
 
+import java.sql.Connection;
+
 /**
  *
  * @author Betty
  */
 public class ExecuteSQL {
+    private Connection con;
     
+    public ExecuteSQL(Connection con){
+        setCon(con);
+    }
+    
+    public Connection getcon(){
+        return con;
+    }
+
+    private void setCon(Connection con) {
+        this.con = con;
+    }    
 }
